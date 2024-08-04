bdd68b123c3b9ecb9f78d2ecdf149b2865cad002
jojindragon <127081849+jojindragon@users.noreply.github.com>
2024-08-04 16:04:28 +0900

    추가 설정 필요
    sdk추가 및 tailwind.config 색 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'custom-blue': '#0074FC',
+      },
     },
   },
   plugins: [],
