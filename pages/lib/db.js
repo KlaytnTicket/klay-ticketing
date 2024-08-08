c0ddba896840eeee08f01649a068d75690375bea
jojindragon <127081849+jojindragon@users.noreply.github.com>
2024-08-08 14:20:34 +0900

    db 연결 예시

---
+++ pages/lib/db.js
@@ -0,0 +1,11 @@
+import { Client } from 'pg';
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+export default client;
