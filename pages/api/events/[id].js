754f3040060c016d8807c31ba6d756ae6a757a87
udongmandu <qjawns10000@naver.com>
2024-08-11 02:32:18 +0900

    티켓 자세히 보기 페이지 추가 및 컴포넌트 화

--- pages/api/events/[id].js
+++ pages/api/events/[id].js
@@ -1,33 +1,35 @@
-import { Client } from "pg";
+import { Client } from 'pg';
 
 export default async function (req, res) {
-	const {id} = req.query;
+  const { id } = req.query;
 
-	const client = new Client({
-		host: process.env.DB_HOST,
-		database: process.env.DB_NAME,
-		port: process.env.DB_PORT,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-	});
-	
+  const client = new Client({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  });
 
-	try {
-		await client.connect();
-		const result = await client.query(
-			'SELECT * FROM "TICKETING_EVENT" WHERE "EVENT_PK" = $1',
-			[id]
-		);
-		await client.end();
+  try {
+    await client.connect();
 
-		if(result.rows.length === 0){
-			res.status(404).json({ error: '이벤트 없다.' });
-		} else{
-			res.status(200).json(result.rows[0]);
-		}
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error: '데이터 패치 실패' });
-	}
-	
+    const EventResult = await client.query('SELECT * FROM "TICKETING_EVENT" WHERE "EVENT_PK" = $1', [id]);
+
+    const TicketResult = await client.query('SELECT * FROM "TICKET" WHERE "EVENT_PK" = $1', [id]);
+
+    await client.end();
+
+    if (EventResult.rows.length === 0) {
+      res.status(404).json({ error: '이벤트 없다.' });
+    } else {
+      res.status(200).json({
+        event: EventResult.rows[0],
+        tickets: TicketResult.rows,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: '데이터 패치 실패' });
+  }
 }
