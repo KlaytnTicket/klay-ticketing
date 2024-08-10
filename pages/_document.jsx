02acd291ad0878029ea8942d9a0461186612ffc4
udongmandu <qjawns10000@naver.com>
2024-08-11 02:31:38 +0900

    topbar 와 footer 추가

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-
+import Topbar from './components/Topbar';
+import Footer from './components/Footer';
 export default function Document() {
   return (
     <Html lang="ko">
       <Head />
+      <Topbar />
       <body>
         <Main />
         <NextScript />
       </body>
+      <Footer />
     </Html>
   );
 }
