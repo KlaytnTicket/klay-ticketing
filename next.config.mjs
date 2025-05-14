469bf63741e76b781cae987f661edcec26c259c7
chauid <dlfdl28@naver.com>
2025-05-14 17:54:52 +0900

    db 이전

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'dsqosoeyathhjxgyduin.supabase.co',
+        hostname: 'bontiphlrkxhwywwppkd.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/klaytnticketing-bucket/**',
       },
